c2793e2628459d90144702fb75a7aa67f5082cce
rpounceyjr <rpounceyjr@gmail.com>
2020-04-18 11:23:28 -0400

    added arrays for waiting and tables, including dummy info.

--- server.js
+++ server.js
@@ -3,7 +3,24 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000
 
-app
+var tableArray = [
+    {
+      customerName: "Ahmed",
+      customerEmail: "ahmed@example.com",
+      customerID: "afhaque89",
+      phoneNumber: "000-000-0000"
+    }
+  ];
+
+  var waitingArray = [
+    {
+      customerName: "Saima",
+      customerEmail: "saima@example.com",
+      phoneNumber: "000-000-0000",
+      customerID: "saimaCool"
+    }
+  ];
+  
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
